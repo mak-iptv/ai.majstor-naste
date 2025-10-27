8df48594671d64afaa53dc46edcc955f8111f7a9
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-10-27 21:15:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 if (!GEMINI_API_KEY) {
-  console.error("GEMINI_API_KEY не е зададен!");
+  console.error("GEMINI_API_KEY");
   process.exit(1);
 }
 
